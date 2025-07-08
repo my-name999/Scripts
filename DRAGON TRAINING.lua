0e0923f3d4540aef865e5f155152272abaf49d7f
my-name999 <minminoaoa4@gmail.com>
2025-07-08 12:39:13 +0300

    DRAGON TRAINING.lua

--- DRAGON TRAINING.lua
+++ DRAGON TRAINING.lua
@@ -1,5 +1,3 @@
-print("Dragon GUI script started")
-
 local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
 local Window = OrionLib:MakeWindow({
 	Name = "🐉 Dragon Training GUI",
